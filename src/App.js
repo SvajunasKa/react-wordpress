8f5a86134852795519086f90b97cc313f90cd2f5
svajunas svajunas <svajunas@aventusgroup.com>
2018-06-26 18:03:28 +0300

    kazkas

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
-    constructor(){
+    constructor() {
         super();
-        this.state= {
+        this.state = {
             pageData: []
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get('http://wordpress.utechna.lt/wp-json/wp/v2/pages')
             .then(res => {
                 this.setState({pageData: res.data})
+                //console.log(res.data[4].content.rendered)
             });
-            console.log(this.state.pageData)
+
     }
 
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-            {this.data}
-        </p>
-      </div>
-    );
-  }
+    render() {
+        let text;
+        if (this.state.pageData.length > 0) {
+            //console.log(this.state.pageData[4].content.rendered);
+            text = this.state.pageData[4].content.rendered;
+        }
+
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    {text}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
